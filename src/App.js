4505d43d478746052a8120ef68938dc120fd29fe
metamn <bartus.csongor@gmail.com>
2019-08-17 08:08:59 +0300

    Adding the CSS baseline reset

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from "react";
 import "typeface-roboto";
 
 import Meta from "./components/Meta";
+
+import CssBaseline from "@material-ui/core/CssBaseline";
 import Button from "@material-ui/core/Button";
 
 function App() {
   return (
     <>
       <Meta />
+      <CssBaseline />
       <Button variant="contained" color="primary">
         Hello World
       </Button>
